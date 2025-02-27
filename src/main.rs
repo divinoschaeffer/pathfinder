5b2e42290bd6972b6c4701ab8b7a9a5181b1ce77
Divino Schaeffer <divino.schaeffer02@gmail.com>
2025-02-28 00:01:32 +0100

    🚧 Init tui

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use crate::maze::Maze;
 use clap::{arg, command, value_parser};
+use color_eyre::Result;
+use ratatui::{DefaultTerminal, Frame};
+use ratatui::crossterm::event;
+use ratatui::crossterm::event::Event;
 
 mod cell;
 mod maze;
 
-fn main() {
+fn main() -> Result<()>{
     let matches = command!()
         .author("Schaeffer Divino, divinoschaeffer@gmail.com")
         .arg(arg!(-d --dimensions <DIM> "Dimensions of the maze")
@@ -19,5 +23,24 @@ fn main() {
         (20, 20)
     };
 
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let result = run(terminal);
     let maze: Maze = Maze::generate_maze(width,height);
+
+    ratatui::restore();
+    result
+}
+
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if matches!(event::read()?, Event::Key(_)) {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
 }
