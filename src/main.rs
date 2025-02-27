30eb55194211add6389a06e70a8a743e5afe6cf3
Divino Schaeffer <divino.schaeffer02@gmail.com>
2025-02-27 23:05:50 +0100

    🎨 Used clap to improve program's arguments handling

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 use crate::maze::Maze;
+use clap::{arg, command, value_parser};
 
 mod cell;
 mod maze;
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let maze: Maze = Maze::generate_maze(20,20);
+    let matches = command!()
+        .author("Schaeffer Divino, divinoschaeffer@gmail.com")
+        .arg(arg!(-d --dimensions <DIM> "Dimensions of the maze")
+            .required(true)
+            .value_parser(value_parser!(usize))
+            .num_args(2))
+        .get_matches();
+
+    let (width, height) = if let Some(mut dims) = matches.get_many::<usize>("dimensions") {
+        (*dims.next().unwrap(), *dims.next().unwrap())
+    } else {
+        (20, 20)
+    };
+
+    let maze: Maze = Maze::generate_maze(width,height);
 }
